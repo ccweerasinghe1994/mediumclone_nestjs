20e852601a3a76739b464a8da946462cf2fb16a3
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-04-11 14:46:48 +0530

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+if (!process.env.IS_TS_NODE) {
+  require('module-alias/register');
+}
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@app/app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Medium API')
+    .setDescription('The Medium API description')
+    .setVersion('1.0')
+    .addTag('medium')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
